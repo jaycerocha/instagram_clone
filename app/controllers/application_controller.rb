001c9a5c46bfbf5b0a7df39894a405c89a6f2bba
jaycerocha <jeyce_jp@hotmail.com>
2020-04-17 13:38:27 -0300

    instalaçao e cofiguraçao do devise
    instalou o devise e devise_i18n
    foi acrescentado os campos name, username na migrate do devise
    foi adicionado validates no model user
    adicionado rotina no controler application_controller para mostrar as chaves name e username no sing_up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    layout :layout_by_resource
+
+    private
+    # toda vez que um controler do devise estiver sendo utilizado ele usar o layout authentication
+    def layout_by_resource
+        "authentication" if devise_controller?
+    end
+    
+    # permite o devise adicione o campo  name e username na tela de registro
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit :sign_up, keys: %i[name username]
+    end
 end
