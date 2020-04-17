d676f60510e753c3c8e8582b6135c95c937b36b1
jaycerocha <jeyce_jp@hotmail.com>
2020-04-17 17:47:20 -0300

    criado view authentication e post  - criado a migrate add_created_by_id_column_to_posts  - acrescentado o relacionamento nos model post e user  - um novo controller user e rotas para user  - criado partial _post  - traduções nas views do devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "posts#index"
-  resources :posts, only: %i[index show new create]
 
+  devise_for :users, only: %i[sessions registrations passwords]
+
+  resources :posts, only: %i[index show new create]
+  resources :users, only: %i[show], param: :username
 end
---
+++ db/migrate/20200417181226_add_created_by_id_column_to_posts.rb
@@ -0,0 +1,6 @@
+class AddCreatedByIdColumnToPosts < ActiveRecord::Migration[6.0]
+  def change
+    add_reference :posts, :created_by, null: false, foreign_key: { to_table: :users }
+  end
+end
+ 
