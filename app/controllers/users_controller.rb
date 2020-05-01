0c73180b01ef5f469323b966f7251e3268f92b3c
jaycerocha <jeyce_jp@hotmail.com>
2020-05-01 14:57:56 -0300

    implementado a funcionalida "sugestões para voce"

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
-   
-    def show
+  include SuggestedUsers
+
+  before_action :set_suggested_users, only: %i[show]
+    
+  def show
       user = User.find_by(username: params[:username])
       @posts = user.posts
   
       render "posts/index"
     end
   
-    
+  
   end
