91a5cb38c10456c396461a1707551d141e8603b1
jaycerocha <jeyce_jp@hotmail.com>
2020-05-01 15:29:43 -0300

    adicionando modal para mostrar opções de estitar perfil e sair implementado a edição de perfil e sair

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i[index show new create]
   resources :users, only: %i[show], param: :username
+  get "edit_profile", to: "users#edit"
+  patch "profile", to: "users#update"
+
 end
