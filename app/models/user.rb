e0f15148c0651511f3703f3a5d9b87983d8033db
jaycerocha <jeyce_jp@hotmail.com>
2020-05-03 17:18:18 -0300

    implementado comentarios     usado websocket para os comentarios e os posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,8 @@ class User < ApplicationRecord
 
   has_many :posts, foreign_key: :created_by_id, dependent: :destroy
   has_many :likes
-
+  has_many :comments
+  
   has_one_attached :avatar
   validates :avatar, content_type: %i[png jpg jpeg]
 end
